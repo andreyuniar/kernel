9d53a50264bcf97a02f73cd769e2c6324a0a61ef
Rick Yiu <rickyiu@google.com>
2019-10-05 15:59:06 +0800

    sched: use sysctl to control energy aware feature
    Energy aware feature control is previously done through debugfs,
    which will be deprecated, so move the control to sysctl.
    
    Bug: 141333728
    Test: function works as expected
    Change-Id: I55411d3bb2669ba1fae3225d67cdf1cf8b3b3a7f
    Signed-off-by: Rick Yiu <rickyiu@google.com>
    (cherry picked from commit c4ea655249385b37b44c235b788be1b6576bc63f)
    Signed-off-by: Panchajanya1999 <panchajanya@azure-dev.live>
    Signed-off-by: Forenche <prahul2003@gmail.com>
    Signed-off-by: andreyuniar <andre.yuniar069@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -139,6 +139,11 @@ unsigned int sysctl_sched_child_runs_first __read_mostly;
 unsigned int __read_mostly sysctl_sched_energy_aware = 1;
 
 /*
+ * To enable/disable energy aware feature.
+ */
+unsigned int __read_mostly sysctl_sched_energy_aware = 1;
+
+/*
  * SCHED_OTHER wake-up granularity.
  *
  * This option delays the preemption effects of decoupled workloads
