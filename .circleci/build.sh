5fca7433d82f17486802caee6c8e744241c6e2f8
andreyuniar <58914618+andreyuniar@users.noreply.github.com>
2021-09-26 16:25:07 +0700

    Update build.sh

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -1,181 +1,82 @@
-#!/bin/bash
-
-DATE=$(date +"%Y%m%d-%H%M")
-
-make_build(){
-
-	print "Make kernel dtb..." green	if [ $TOOLCHAIN == clang ]; then
-
-		export KBUILD_COMPILER_STRING=$(clang/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
-
-		PATH="$(pwd)/clang/bin:${PATH}"
-
-		make O=out ARCH=arm64 vendor/surya_defconfig
-
-		make -j$(nproc --all) O=out \
-
-                      ARCH=arm64 \
-
-                      CC=clang \
-
-                      LD=ld.lld \
-
-                      CROSS_COMPILE=aarch64-linux-gnu- \
-
-                      CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
-
-                      NM=llvm-nm \
-
-                      OBJCOPY=llvm-objcopy \
-
-                      OBJDUMP=llvm-objdump \
-
-                      STRIP=llvm-strip
-
-	else
-
-		export CROSS_COMPILE=$(pwd)/gcc64/bin/aarch64-elf-
-
-		export CROSS_COMPILE_ARM32=$(pwd)/gcc32/bin/arm-eabi-
-
-		make O=out ARCH=arm64 $1
-
-		make -j$(nproc --all) O=out ARCH=arm64
-
-	fi
-
+#!/usr/bin/env bash
+echo "Cloning dependencies"
+git clone --depth=1 https://github.com/kdrag0n/proton-clang clang
+git clone --depth=1 git clone --depth=1 https://github.com/andreyuniar/AnyKernel33 AnyKernel
+echo "Done"
+IMAGE=$(pwd)/out/arch/arm64/boot/Image.gz-dtb
+IMAGE=$(pwd)/out/arch/arm64/boot/dtbo.img
+TANGGAL=$(date +"%F-%S")
+START=$(date +"%s")
+KERNEL_DIR=$(pwd)
+PATH="${PWD}/clang/bin:$PATH"
+export KBUILD_COMPILER_STRING="$(${KERNEL_DIR}/clang/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')"
+export ARCH=arm64
+export KBUILD_BUILD_HOST=circleci
+export KBUILD_BUILD_USER="andreyuniar"
+# sticker plox
+function sticker() {
+    curl -s -X POST "https://api.telegram.org/bot$token/sendSticker" \
+        -d sticker="CAACAgEAAxkBAAEnKnJfZOFzBnwC3cPwiirjZdgTMBMLRAACugEAAkVfBy-aN927wS5blhsE" \
+        -d chat_id=$chat_id
 }
-
-make_clean(){
-
-	rm -rf out
-
+# Send info plox channel
+function sendinfo() {
+    curl -s -X POST "https://api.telegram.org/bot$token/sendMessage" \
+        -d chat_id="$chat_id" \
+        -d "disable_web_page_preview=true" \
+        -d "parse_mode=html" \
+        -d text="<b>• Excalibur Kernel •</b>%0ABuild started on <code>Circle CI</code>%0AFor device <b>Xiaomi Redmi Note7/7S</b> (lavender)%0Abranch <code>$(git rev-parse --abbrev-ref HEAD)</code>(master)%0AUnder commit <code>$(git log --pretty=format:'"%h : %s"' -1)</code>%0AUsing compiler: <code>${KBUILD_COMPILER_STRING}</code>%0AStarted on <code>$(date)</code>%0A<b>Build Status:</b>#Stable"
 }
-
-send_msg(){
-
-	print "${1}" green
-
-	curl -s -X POST https://api.telegram.org/bot"${bot_token}"/sendMessage \
-
-		-d parse_mode="Markdown" \
-
-		-d chat_id="${chat_id}" \
-
-		-d text="${1}"
-
+# Push kernel to channel
+function push() {
+    cd AnyKernel
+    ZIP=$(echo *.zip)
+    curl -F document=@$ZIP "https://api.telegram.org/bot$token/sendDocument" \
+        -F chat_id="$chat_id" \
+        -F "disable_web_page_preview=true" \
+        -F "parse_mode=html" \
+        -F caption="Build took $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) second(s). | For <b>Xiaomi Redmi Note 7/7s (lavender)</b> | <b>$(${GCC}gcc --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')</b>"
 }
-
-send_file(){
-
-	print "Sending file..." green
-
-	SECONDS=$SECONDS
-
-	if (( $SECONDS > 0 )) ; then
-
-	   curl -F chat_id="${chat_id}" \
-
-		-F caption="" \
-
-		-F document=@"${1}" \
-
-		 https://api.telegram.org/bot"${bot_token}"/sendDocument 
-
-	fi
-
+# Fin Error
+function finerr() {
+    curl -s -X POST "https://api.telegram.org/bot$token/sendMessage" \
+        -d chat_id="$chat_id" \
+        -d "disable_web_page_preview=true" \
+        -d "parse_mode=markdown" \
+        -d text="Build throw an error(s)"
+    exit 1
 }
+# Compile plox
+function compile() {
+    make O=out ARCH=arm64 surya_defconfig
+    make -j$(nproc --all) O=out \
+                    ARCH=arm64 \
+                    CC=clang \
+                    LD=ld.lld \
+                    CROSS_COMPILE=aarch64-linux-gnu- \
+                    CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
+                    NM=llvm-nm \
+                    OBJCOPY=llvm-objcopy \
+                    OBJDUMP=llvm-objdump \
+                    STRIP=llvm-strip
 
-mkzip(){
-
-	print "Generate zip file..." green
-
-	if [ -f $(pwd)/out/arch/arm64/boot/Image.gz-dtb ]; then
-
-		cp $(pwd)/out/arch/arm64/boot/Image.gz-dtb  anykernel
-
-                cp $(pwd)/out/arch/arm64/boot/dtbo.img  anykernel
-
-		cd anykernel
-
-		zip -r9 ../$1 * -x .git README.md *placeholder
-
-		cd ..
-
-		send_file $1
-
-	else
-
-		send_msg "Build Failed !"
-
-		print "Build error !" red
-
-		exit 1
-
-	fi
-
+    if ! [ -a "$IMAGE" ]; then
+        finerr
+        exit 1
+    fi
+    cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
+    cp out/arch/arm64/boot/dtbo.img AnyKernel
 }
-
-print(){
-
-	echo ""
-
-	case ${2} in
-
-		"red")
-
-		echo -e "\033[31m $1 \033[0m";;
-
-		"green")
-
-		echo -e "\033[32m $1 \033[0m";;
-
-		*)
-
-		echo $1
-
-		;;
-
-	esac
-
+# Zipping
+function zipping() {
+    cd AnyKernel || exit 1
+    zip -r9 Excalibur-Surya-${TANGGAL}.zip *
+    cd ..
 }
-
-gen_toolchain(){
-
-	print "Cloning toolchain files..." green
-
-	if [ $TOOLCHAIN == "clang" ]
-
-	then
-
-		git clone --depth=1 https://github.com/kdrag0n/proton-clang clang
-
-	else
-
-		git clone --depth=1 https://github.com/chips-project/aarch64-elf gcc64
-
-		git clone --depth=1 https://github.com/chips-project/arm-eabi gcc32
-
-	fi
-
-	git clone --depth=1 https://github.com/andreyuniar/AnyKernel33 -b master anykernel
-
-}
-
-build(){
-
-	export type="${1} ${2} ${3}"
-
-	make_clean
-
-	if [ $2 == "-OSS" ]
-
-	then
-
-		make_build "${1}-surya_defconfig"
-
-	fi
-
-		mkzip "$KERNELNAME-Surya-OSS-$DATE.zip"
-
-}
+sticker
+sendinfo
+compile
+zipping
+END=$(date +"%s")
+DIFF=$(($END - $START))
+push
